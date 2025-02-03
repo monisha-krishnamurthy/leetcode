bc9632ecdc287704efd5991dfcd1230fdcea5144
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-02-03 16:41:43 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (39.67%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        dp = [0 for i in range(n+1)]
+        if n==0: return 0
+        if n==1: return 1
+        if n==2: return 1
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 1
+
+        for i in range(3,n+1):
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+        print(dp)
+        return dp[n]
