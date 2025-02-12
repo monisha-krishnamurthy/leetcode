6baa62424d69c043c8f1cbbb8e45fa7a1ac5c207
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-02-12 11:32:18 -0700

    Time: 48 ms (81.10%) | Memory: 49.3 MB (22.10%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
@@ -5,27 +5,39 @@
 #         self.next = next
 
 class Solution:
-    def ll_length(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        length = 0
-        temp = head
-        while temp:
-            length +=1
-            temp = temp.next
-        return length 
+    # def ll_length(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    #     length = 0
+    #     temp = head
+    #     while temp:
+    #         length +=1
+    #         temp = temp.next
+    #     return length 
 
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
             return None
 
-        leng = self.ll_length(head)
-        temp = head
+        slow = head
+        fast = head
         prev = None
 
-        for _ in range(leng//2):
-            prev = temp
-            temp = temp.next
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
         
-        if prev:
-            prev.next = temp.next
+        prev.next = slow.next
 
         return head
+        # leng = self.ll_length(head)
+        # temp = head
+        # prev = None
+
+        # for _ in range(leng//2):
+        #     prev = temp
+        #     temp = temp.next
+        
+        # if prev:
+        #     prev.next = temp.next
+
+        # return head
