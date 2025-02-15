96b8b40d8bd8f4e326cec9b6b533eac4d8103512
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-02-15 14:08:58 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (60.13%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.py
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -1,22 +1,22 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        digits = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+        digits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         if k>n:
             return []
         
-        def combinations(k, n, digits, output_set, output=[]):
+        def combinations(k, n, digits_index, digits, output_list, output=[]):
             if n == 0 and k == 0:
-                output.sort()
-                output_set.add(tuple(output))
+                output_list.append(output)
                 return
             elif (n !=0 and k==0) or (n==0 and k!=0):
                 return 
 
-            for digit in digits:
+            for index in range(digits_index, 9):
+                digit = digits[index]
                 if digit <= n:
-                    combinations(k-1, n-digit, digits-{digit}, output_set, output+[digit])
+                    combinations(k-1, n-digit, index+1, digits, output_list, output+[digit])
 
-        output_set = set()
-        combinations(k, n, digits, output_set)
-        return list(output_set)
+        output_list = []
+        combinations(k, n, 0, digits, output_list)
+        return list(output_list)
             
