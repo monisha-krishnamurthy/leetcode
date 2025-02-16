478df85882b3f39b78e46e1184251e91d46ee804
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-02-15 20:22:07 -0700

    Time: 160 ms (17.02%) | Memory: 32.2 MB (63.22%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -14,11 +14,10 @@ class Solution:
                 return 
             
             if root.val >= max_value:
-                max_value = root.val
                 good_nodes +=1
  
-            recursive(root.left, max_value)
-            recursive(root.right, max_value)
+            recursive(root.left, max(max_value, root.val))
+            recursive(root.right, max(max_value, root.val))
 
         recursive(root, float('-inf'))
         return good_nodes
