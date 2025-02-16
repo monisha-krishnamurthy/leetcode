81593eb4a64d527fc127d44324a29152b179a3b4
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-02-16 15:05:42 -0700

    Time: 721 ms (9.52%) | Memory: 38.2 MB (90.97%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
@@ -5,23 +5,24 @@ class Solution:
         potions.sort()
         for i,spell in enumerate(spells):
             div = math.ceil(success/spell)
-            index = bisect.bisect_left(potions, div)
-            pairs[i] = len(potions) - index
+            # index = bisect.bisect_left(potions, div)
+            # pairs[i] = len(potions) - index
 
-            # left = 0
-            # right = len(potions)-1
-            # while left<=right:
-            #     mid = math.floor(int((left+right)/2))
-            #     if potions[mid] < div:
-            #         left = mid+1
-            #     elif potions[mid] == div:
-            #         pairs[i] = len(potions)-left
-            #         # while mid-1>=0 and potions[mid-1] == div:
-            #         #     mid = mid-1
-            #         #     pairs[i] +=1
-            #         break
-            #     elif potions[mid] > div:
-            #         pairs[i] = len(potions)-mid
-            #         right = mid-1
+            left = 0
+            right = len(potions)-1
+            while left<=right:
+                mid = left+(right-left)//2
+                if potions[mid] < div:
+                    left = mid+1
+                elif potions[mid] == div:
+                    pairs[i] = len(potions)-mid
+                    # while mid-1>=0 and potions[mid-1] == div:
+                    #     mid = mid-1 
+                    #     pairs[i] +=1
+                    # break
+                    right = mid-1
+                elif potions[mid] > div:
+                    pairs[i] = len(potions)-mid
+                    right = mid-1 
         
         return pairs
