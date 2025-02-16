f226a0758fd0ac0ed826a47824a27ba609769215
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-02-15 20:19:32 -0700

    Time: 274 ms (5.27%) | Memory: 32.1 MB (80.99%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -7,19 +7,20 @@
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
 
-        def recursive(root, max_value, good_nodes):
+        good_nodes = 0
+        def recursive(root, max_value):
+            nonlocal good_nodes
             if root is None:
                 return 
             
             if root.val >= max_value:
-                good_nodes[0] +=1
+                good_nodes +=1
  
-            recursive(root.left, max(max_value, root.val), good_nodes)
+            recursive(root.left, max(max_value, root.val))
             print(good_nodes, "1st")
 
-            recursive(root.right, max(max_value, root.val), good_nodes)
+            recursive(root.right, max(max_value, root.val))
             print(good_nodes, "2nd")
 
-        good_nodes = [0]
-        recursive(root, float('-inf'), good_nodes)
-        return good_nodes[0]
+        recursive(root, float('-inf'))
+        return good_nodes
