8ecd7291479f5716ecbd28fe4d471c9d4e4fbf52
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-02-16 19:57:30 -0700

    Time: 23 ms (6.18%) | Memory: 18.5 MB (14.45%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -1,18 +1,46 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        for i in range(len(flowerbed)):
-            if flowerbed[i] == 0:
-                empty_left_plot = (i==0) or (flowerbed[i-1]==0)
-                empty_right_plot = (i==len(flowerbed)-1) or (flowerbed[i+1]==0)
+        # for i in range(len(flowerbed)):
+        #     if flowerbed[i] == 0:
+        #         empty_left_plot = (i==0) or (flowerbed[i-1]==0)
+        #         empty_right_plot = (i==len(flowerbed)-1) or (flowerbed[i+1]==0)
             
-                if empty_left_plot and empty_right_plot:
-                    flowerbed[i] = 1
-                    n -= 1
-        if n>0:
-            return False
-        else:
+        #         if empty_left_plot and empty_right_plot:
+        #             flowerbed[i] = 1
+        #             n -= 1
+        # if n>0:
+        #     return False
+        # else:
+        #     return True
+
+        if n==0:
+            return True
+        elif len(flowerbed)==1 and flowerbed[0]==0:
             return True
 
+        count = 0
+        for i in range(len(flowerbed)):
+            if flowerbed[0] == 0 and flowerbed[1] ==0:
+                count +=1
+                flowerbed[0] = 1
+                print(flowerbed)
+                print(count, "1st")
+            if (i>0 and i<len(flowerbed)-1) and flowerbed[i]==0:
+                if flowerbed[i-1]==0 and flowerbed[i+1]==0:
+                    flowerbed[i] = 1
+                    count +=1
+                    print(count, "2nd")
+                    continue
+            if len(flowerbed)>2 and (flowerbed[-1] ==0 and flowerbed[-2]==0):
+                count +=1
+                flowerbed[-1] = 1
+                print(flowerbed)
+                print(count, "3rd")
+
+        if count >= n:
+            return True
+        else:
+            return False
 
                             
         
