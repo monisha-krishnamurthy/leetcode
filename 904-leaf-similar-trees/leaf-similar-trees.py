d77a71626b6e2842f5d012d2be3e22708e365d67
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-02-17 15:22:46 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (74.43%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.py
+++ 904-leaf-similar-trees/leaf-similar-trees.py
@@ -23,9 +23,19 @@ class Solution:
                     if node.right is not None:
                         stack.append(node.right)
             return out
-    
-        out1 = getLeafSeq(root1)
-        out2 = getLeafSeq(root2)
+        
+        def getLeafSeqRecursive(root, output):
+            if root.left is None and root.right is None:
+                output.append(root.val)
+                return
+            
+            if root.left is not None:
+                getLeafSeqRecursive(root.left, output)
+            if root.right is not None:
+                getLeafSeqRecursive(root.right, output)
+
+        getLeafSeqRecursive(root1, out1)
+        getLeafSeqRecursive(root2, out2)
         if out1 == out2:
             return True
         return False
