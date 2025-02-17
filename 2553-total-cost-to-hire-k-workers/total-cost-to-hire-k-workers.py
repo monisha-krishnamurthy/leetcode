6a07ad2b77a3eb0c4852e8c7d09b54de06c673a9
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-02-17 14:23:10 -0700

    Time: 347 ms (8.17%) | Memory: 41.8 MB (6.06%) - LeetSync

--- 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.py
+++ 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.py
@@ -11,6 +11,13 @@ class Solution:
         
         cost = 0
         for i in range(k):
+            if len(heap2) < candidates and left_index < right_index - 1:
+                    heapq.heappush(heap2, (costs[right_index - 1], right_index - 1))
+                    right_index = right_index - 1
+            if len(heap1) < candidates and left_index < right_index - 1:
+                    heapq.heappush(heap1, (costs[left_index + 1], left_index + 1))
+                    left_index = left_index + 1
+            
             if len(heap1) == 0:
                 ele1 = float('inf')
             else:
@@ -19,25 +26,18 @@ class Solution:
                 ele2 = float('inf')
             else:
                 ele2, index2 = heap2[0]
+                
             if ele1 > ele2:
                 heapq.heappop(heap2)
                 cost += ele2
-                if len(heap2) < candidates and left_index < right_index - 1:
-                    heapq.heappush(heap2, (costs[right_index - 1], right_index - 1))
-                    right_index = right_index - 1
             elif ele2 > ele1:
                 heapq.heappop(heap1)
                 cost += ele1
-                if len(heap1) < candidates and left_index < right_index - 1:
-                    heapq.heappush(heap1, (costs[left_index + 1], left_index + 1))
-                    left_index = left_index + 1
+                
             else:
                 if index1 < index2:
                     heapq.heappop(heap1)
                     cost += ele1
-                    if len(heap1) < candidates and left_index < right_index - 1:
-                        heapq.heappush(heap1, (costs[left_index + 1], left_index + 1))
-                        left_index = left_index + 1
                 elif index1 == index2:
                     heapq.heappop(heap1)
                     heapq.heappop(heap2)
