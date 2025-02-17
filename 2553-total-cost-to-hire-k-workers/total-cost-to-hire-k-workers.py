1797eba70a07894b18825ac284b0676d30478190
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-02-17 14:24:23 -0700

    Time: 159 ms (48.66%) | Memory: 27.7 MB (70.17%) - LeetSync

--- 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.py
+++ 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.py
@@ -1,45 +1,28 @@
 class Solution:
-    def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
-        heap1 = []
-        heap2 = []
-        for index, cost in enumerate(costs[:candidates]):
-            heapq.heappush(heap1, (cost, index))
-            left_index = index
-        for index in range(len(costs)-1, len(costs)-1-candidates, -1):
-            heapq.heappush(heap2, (costs[index], index))
-            right_index = index
-        
-        cost = 0
-        for i in range(k):
-            if len(heap2) < candidates and left_index < right_index - 1:
-                    heapq.heappush(heap2, (costs[right_index - 1], right_index - 1))
-                    right_index = right_index - 1
-            if len(heap1) < candidates and left_index < right_index - 1:
-                    heapq.heappush(heap1, (costs[left_index + 1], left_index + 1))
-                    left_index = left_index + 1
-            
-            if len(heap1) == 0:
-                ele1 = float('inf')
+    def totalCost(self, costs, k, candidates):
+        i = 0
+        j = len(costs) - 1
+        pq1 = []
+        pq2 = []
+
+        ans = 0
+        while k > 0:
+            while len(pq1) < candidates and i <= j:
+                heapq.heappush(pq1, costs[i])
+                i += 1
+            while len(pq2) < candidates and i <= j:
+                heapq.heappush(pq2, costs[j])
+                j -= 1
+
+            t1 = pq1[0] if pq1 else float('inf')
+            t2 = pq2[0] if pq2 else float('inf')
+
+            if t1 <= t2:
+                ans += t1
+                heapq.heappop(pq1)
             else:
-                ele1, index1 = heap1[0]
-            if len(heap2) == 0:
-                ele2 = float('inf')
-            else:
-                ele2, index2 = heap2[0]
-                
-            if ele1 > ele2:
-                heapq.heappop(heap2)
-                cost += ele2
-            elif ele2 > ele1:
-                heapq.heappop(heap1)
-                cost += ele1
-                
-            else:
-                if index1 < index2:
-                    heapq.heappop(heap1)
-                    cost += ele1
-                elif index1 == index2:
-                    heapq.heappop(heap1)
-                    heapq.heappop(heap2)
-                    cost += ele1
-        return cost
+                ans += t2
+                heapq.heappop(pq2)
+
+            k -= 1
+        return ans
