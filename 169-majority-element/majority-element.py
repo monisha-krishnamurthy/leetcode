9538b1a3366861eaf78a5abd9867caa83644c88b
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-02-19 20:30:24 -0700

    Time: 10 ms (32.80%) | Memory: 19.2 MB (87.25%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,14 +4,14 @@ class Solution:
         candidate = nums[0]
 
         for ele in nums:
-            # if count == 0:
-            #     candidate = ele
+            if count == 0:
+                candidate = ele
 
             if ele == candidate:
                 count += 1
             else:
                 count -= 1
-                if count == 0:
-                    candidate = ele
-                    count = 1
+                # if count == 0:
+                #     candidate = ele
+                #     count = 1
         return candidate
