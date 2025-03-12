a66e45e727e8741eda6cf0e312ba4865eae40d94
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-03-11 23:44:33 -0700

    Time: 148 ms (49.49%) | Memory: 78.7 MB (27.22%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,32 +1,43 @@
-from collections import deque
-class LRUCache:
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev, self.next = None, None
 
+class LRUCache:
     def __init__(self, capacity: int):
-        self.dictionary = dict()
-        self.queue = deque()
         self.capacity = capacity
+        self.cache = {}
+        self.left, self.right = Node(0,0), Node(0,0)
+        self.left.next, self.right.prev = self.right, self.left
+
+    def remove(self, node: Node):
+        previous_node, next_node = node.prev, node.next
+        previous_node.next = next_node
+        next_node.prev = previous_node
+
+    def insert(self, node: Node):
+        previous_node, next_node = self.right.prev, self.right
+        previous_node.next, next_node.prev = node, node
+        node.next, node.prev = next_node, previous_node
 
     def get(self, key: int) -> int:
-        if key in self.dictionary:
-            self.queue.remove(key)
-            self.queue.append(key)
-            return self.dictionary[key]
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].value
         return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.dictionary:
-            self.queue.remove(key)
-            self.queue.append(key)
-            self.dictionary[key] = value
-        else:
-            if len(self.queue) < self.capacity:
-                self.queue.append(key)
-                self.dictionary[key] = value
-            else:
-                key_to_be_deleted = self.queue.popleft()
-                del self.dictionary[key_to_be_deleted]
-                self.queue.append(key)
-                self.dictionary[key] = value
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+    
+        if len(self.cache) > self.capacity:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key] 
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
