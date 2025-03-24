9a0c9d651debc3fa3e4586bd528b2557decf8e5f
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-03-23 20:43:10 -0700

    Time: 59 ms (30.29%) | Memory: 22.1 MB (44.65%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -8,31 +6,14 @@ from collections import deque
 #         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':  
-        def recursive(node, node_search):
-            if node is None: 
-                return []
-            if node.val == node_search.val:
-                return [node]
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None or root == p or root == q:
+            return root
 
-            path2 =  []
-            path1 = recursive(node.left, node_search)
-            if len(path1)== 0:
-                path2 = recursive(node.right, node_search) 
-            if not path1 and not path2:
-                return []
-            else:
-                return [node] + path1 + path2 
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
 
-        out1 = recursive(root, p)
-        out2 = recursive(root, q)
-        while len(out1) < len(out2):
-            out2.pop()
-        while len(out2) < len(out1):
-            out1.pop()
-        
-        while True:
-            ele1 = out1.pop()
-            ele2 = out2.pop()
-            if ele1 == ele2:
-                return ele1
+        if left and right:
+            return root
+        return left if left else right 
+
