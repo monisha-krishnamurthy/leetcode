69a8fd9dc3e345b32bf56fa5cd14c5038a8b78b6
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-03-26 16:12:29 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (69.94%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        output_list = []
+        self.recursive(n, 0, "", output_list)
+        return output_list
+
+    def recursive(self, no_of_pairs, brackets_to_be_closed, output, output_list):
+        if no_of_pairs == 0 and brackets_to_be_closed == 0:
+            output_list.append(output)
+            return 
+
+        if brackets_to_be_closed == 0:
+            self.recursive(no_of_pairs - 1, brackets_to_be_closed + 1, output + "(", output_list)
+        if no_of_pairs == 0:
+            self.recursive(no_of_pairs, brackets_to_be_closed - 1, output + ")", output_list)
+        if no_of_pairs != 0 and brackets_to_be_closed != 0:
+            self.recursive(no_of_pairs - 1, brackets_to_be_closed + 1, output + "(", output_list)
+            self.recursive(no_of_pairs, brackets_to_be_closed - 1, output + ")", output_list) 
+
