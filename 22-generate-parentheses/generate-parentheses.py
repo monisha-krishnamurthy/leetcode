9c59cee1354c34005a9118e9a5b1b4638994df30
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-03-26 16:14:08 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (44.13%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -9,11 +9,11 @@ class Solution:
             output_list.append(output)
             return 
 
-        if brackets_to_be_closed == 0:
-            self.recursive(no_of_pairs - 1, brackets_to_be_closed + 1, output + "(", output_list)
-        if no_of_pairs == 0:
+        if brackets_to_be_closed != 0:
             self.recursive(no_of_pairs, brackets_to_be_closed - 1, output + ")", output_list)
-        if no_of_pairs != 0 and brackets_to_be_closed != 0:
-            self.recursive(no_of_pairs - 1, brackets_to_be_closed + 1, output + "(", output_list)
-            self.recursive(no_of_pairs, brackets_to_be_closed - 1, output + ")", output_list) 
+        if no_of_pairs != 0:
+            self.recursive(no_of_pairs-1, brackets_to_be_closed + 1, output + "(", output_list)
+        # if no_of_pairs != 0 and brackets_to_be_closed != 0:
+        #     self.recursive(no_of_pairs - 1, brackets_to_be_closed + 1, output + "(", output_list)
+        #     self.recursive(no_of_pairs, brackets_to_be_closed - 1, output + ")", output_list) 
 
