0c04a280b00bb0d2e65cc29e04a342f916117b45
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-04-06 13:13:05 -0700

    Time: 6 ms (56.09%) | Memory: 21.2 MB (80.48%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -4,9 +4,15 @@ class Solution:
         for ele in nums:
             freq_map[ele] += 1
 
-        sorted_list = sorted(freq_map.items(), key=lambda x:x[1], reverse = True) 
+        # to append key,value of hashmap into a tuple
+        # for k, v in my_dict.items():   //use .items() to unpack k,v pairs in a hashmap
+        #     tuple_list.append((k, v))
 
-        output = list()
+        # sorting a dictionary based on its values whose output will be a tuple
+        sorted_list = sorted(freq_map.items(), key=lambda x:x[1], reverse = True) 
+        # print(sorted_list) ---- [(1, 3), (2, 2), (3, 1)] for input [1,1,1,2,2,3]
+
+        output = list() 
         for i in range(k):
             output.append(sorted_list[i][0])
         return output
