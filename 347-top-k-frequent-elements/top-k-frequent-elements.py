8e6ef4ee88fa9971dfaf54e511afc507aabbe86d
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-04-06 12:58:30 -0700

    Time: 4 ms (70.03%) | Memory: 21.2 MB (61.89%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,16 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq_map = defaultdict(int)
+        for ele in nums:
+            freq_map[ele] += 1
+        
+        pair_list = list()
+        for key,val in freq_map.items():
+            pair_list.append((key,val))
+
+        sorted_list = sorted(pair_list, key=lambda x:x[1], reverse = True) 
+
+        output = list()
+        for i in range(k):
+            output.append(sorted_list[i][0])
+        return output
