03c07ea872be367bc7a3e414c538f155bcf459b1
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-04-06 14:23:18 -0700

    Time: 3 ms (90.94%) | Memory: 21.2 MB (80.48%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,18 +1,18 @@
 class Solution:
+    import heapq
+    # USING FREQ MAP
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freq_map = defaultdict(int)
-        for ele in nums:
-            freq_map[ele] += 1
+        for num in nums:
+            freq_map[num] +=1
 
-        # to append key,value of hashmap into a tuple
-        # for k, v in my_dict.items():   //use .items() to unpack k,v pairs in a hashmap
-        #     tuple_list.append((k, v))
+        heap = list()
+        for key,val in freq_map.items():
+            heapq.heappush(heap, (-val, key))
+        print(heap)
 
-        # sorting a dictionary based on its values whose output will be a tuple
-        sorted_list = sorted(freq_map.items(), key=lambda x:x[1], reverse = True) 
-        # print(sorted_list) ---- [(1, 3), (2, 2), (3, 1)] for input [1,1,1,2,2,3]
-
-        output = list() 
+        output = list()
         for i in range(k):
-            output.append(sorted_list[i][0])
+            freq, num = heapq.heappop(heap)
+            output.append(num)
         return output
