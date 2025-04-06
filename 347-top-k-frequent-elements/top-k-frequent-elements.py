08e9452dea575a98a76a6809a1e33f21519e193d
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-04-06 13:05:59 -0700

    Time: 6 ms (56.09%) | Memory: 21.3 MB (61.89%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -3,10 +3,6 @@ class Solution:
         freq_map = defaultdict(int)
         for ele in nums:
             freq_map[ele] += 1
-        
-        # pair_list = list()
-        # for key,val in freq_map.items():
-        #     pair_list.append((key,val))
 
         sorted_list = sorted(freq_map.items(), key=lambda x:x[1], reverse = True) 
 
