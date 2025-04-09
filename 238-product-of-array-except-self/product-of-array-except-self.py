726fae1dff71010143efe188b2e654f6241609a6
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-04-09 14:51:07 -0700

    Time: 36 ms (21.65%) | Memory: 27 MB (11.61%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,24 +1,18 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # using prefix and suffix arrays
         res = [0]*len(nums)
-
-        total = 1
-        cnt_zero = 0
-        for num in nums:
-            if num != 0:
-                total *= num
-            else:
-                cnt_zero += 1
-
+        prefix = [0]*len(nums)
+        suffix = [0]*len(nums)
+        
+        prefix[0] = suffix[len(nums)-1] = 1
+        for i in range(1, len(nums)):
+            prefix[i] = prefix[i-1] * nums[i-1]
+        for i in range(len(nums)-2, -1, -1):
+            suffix[i] = suffix[i+1] * nums[i+1]
         for i in range(len(nums)):
-            if cnt_zero == 0:
-                res[i] = total // nums[i]
-            else:
-                if cnt_zero == 1:
-                    if nums[i] != 0:
-                        res[i] = 0
-                    else:
-                        res[i] = total 
-                elif cnt_zero > 1:
-                    res[i] = 0
+            res[i] = prefix[i] * suffix[i]
         return res 
+        
+# TIME COMPLEXITY: O(n)
+# SPACE COMPLEXITY: O(n)
