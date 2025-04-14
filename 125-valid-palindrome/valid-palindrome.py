d58b85cf7468cfad6ad173b4aaa15810cee24a00
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-04-14 11:07:44 -0700

    Time: 23 ms (6.75%) | Memory: 17.9 MB (95.33%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,15 +1,18 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        string = ''
-        # ord(char):Takes a character and returns its ASCII number.
-        # chr(number):Takes an ASCII number and returns its character.
-        for ch in s:
-            if 'A' <= ch <= 'Z':
-                ch = chr(ord(ch) + 32)  # convert uppercase to lowercase
-                string += ch
-            elif 'a' <= ch <= 'z' or '0' <= ch <= '9':
-                string += ch
-            # skip non-alphabetic characters
+        l, r = 0, len(s)-1
 
-        print(string)
-        return string == string[::-1]  # check if string is palindrome
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while l < r and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        return True
+
+    def alphaNum(self, ch):
+        return (ord('A') <= ord(ch) <= ord('Z') or
+                ord('a') <= ord(ch) <= ord('z') or
+                ord('0') <= ord(ch) <= ord('9') )
