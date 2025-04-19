77781464fd8554a989d1da115cd737083de89150
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-04-19 15:29:37 -0700

    Time: 527 ms (76.65%) | Memory: 20.7 MB (69.44%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,31 +1,28 @@
 class Solution:
-    # BRUTE FORCE SOLUTION
+    # HASH-MAP SOLUTION 
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        count_dict = defaultdict(int)
-        for num in nums:
-            count_dict[num] += 1
-
         res = []
-        for i in range(len(nums)):
-            count_dict[nums[i]] -= 1
-            if i and nums[i] == nums[i - 1]:
+
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i - 1]:
                 continue
 
-            for j in range(i+1, len(nums)):
-                count_dict[nums[j]] -= 1
-                if j - 1 > i and nums[j] == nums[j - 1]:
-                    continue
-                search = -(nums[i] + nums[j]) 
-                if count_dict[search] > 0:
-                    res.append([nums[i], nums[j], search])
-
-            for j in range(i + 1, len(nums)):
-                count_dict[nums[j]] += 1
+            l, r = i + 1, len(nums)-1
+            while l < r:
+                threeSum = a + nums[l] + nums[r] 
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
         return res
-
-# TIME-COMPLEXITY: O(n^3)
-# SPACE-COMPLEXITY: O(n) 
+# TIME-COMPLEXITY: O(nlogN) + O(n^2) = O(n^2)
+# SPACE-COMPLEXITY: O(m) for output list 
 
 
 
