2b6175652a5e5da88cc2e1cc3a686d211e8ab7d1
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-04-21 21:15:11 -0400

    Time: 4 ms (4.63%) | Memory: 17.9 MB (65.14%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        output = []
+        def backtracking(current_index, out):
+            if current_index >= len(nums):
+                output.append(out)
+                return 
+
+            backtracking(current_index+1, out)
+            backtracking(current_index+1, out + [nums[current_index]])
+
+        backtracking(0, [])
+        return output
+        
