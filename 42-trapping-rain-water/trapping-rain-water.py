c84f818ed2335b76d0ecd8d6366417799c191379
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-04-23 14:31:50 -0400

    Time: 35 ms (13.49%) | Memory: 19.4 MB (40.53%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -5,14 +5,12 @@ class Solution:
         for i in range(len(height)):
             int_max = max(int_max, height[i])
             max_front[i] = int_max
-        print(max_front)
 
         max_back = [0]*len(height)
         back_max = height[len(height)-1]
         for i in range(len(height)-1, -1, -1):
             back_max = max(back_max, height[i])
             max_back[i] = back_max
-        print(max_back)
         
         output = 0
         for i in range(len(height)):
