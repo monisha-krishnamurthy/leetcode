d2e62e75ce282c9def175d3e91b14149fdb8e914
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-04-25 17:53:46 -0400

    Time: 15 ms (81.94%) | Memory: 17.8 MB (53.09%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,17 +1,19 @@
 class Solution:
+    #USING SLIDING WINDOW (TWO-POINTERS)
     def lengthOfLongestSubstring(self, s: str) -> int:
         max_length = 0
+        charSet = set()
+        l = 0
 
-        if len(s) == 1:
-            return 1
-
-        for i in range(len(s)):
-            charSet = set()
-            for j in range(i, len(s)):
-                if s[j] in charSet:                 
-                    break
-                charSet.add(s[j]) 
-            max_length = max(max_length, len(charSet))
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r]) 
+            max_length = max(max_length, r - l + 1)
         return max_length
 
+
+#TIME-COMPLEXITY: O(n*m) where m is the total no. of unique chars in the array
+#SPACE-COMPLEXITY: O(m)
 
