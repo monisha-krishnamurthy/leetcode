0bd939e28d00957a990be4a6eead1cdf3e94f531
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-04-28 13:48:55 -0700

    Time: 33 ms (42.03%) | Memory: 17.8 MB (88.00%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -5,12 +5,12 @@ class Solution:
             freq_s1[ch] += 1
 
         l = 0
-        r = len(s1) 
-        substring = s2[l : r]
+        substring = s2[l : len(s1)]
         for ch in substring:
             if ch in freq_s1:
                 freq_s1[ch] -= 1
-        while r < len(s2):
+
+        for r in range(len(s1), len(s2)):
             if all(val == 0 for val in freq_s1.values()):
                 return True
             if s2[l] in freq_s1:
@@ -18,7 +18,6 @@ class Solution:
             if s2[r] in freq_s1:
                 freq_s1[s2[r]] -= 1
             l += 1
-            r += 1
         if all(val == 0 for val in freq_s1.values()):
             return True
         return False
