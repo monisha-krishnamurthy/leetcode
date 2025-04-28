9064a4416ea81212acdd2b06e0d2a4cc5889d29b
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-04-27 20:04:16 -0700

    Time: 15 ms (85.47%) | Memory: 18 MB (39.81%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -3,14 +3,39 @@ class Solution:
         freq_s1 = defaultdict(int)
         for ch in s1:
             freq_s1[ch] += 1
+        print(freq_s1)
 
-        k = len(s1) 
+        l = 0
+        r = len(s1)
+        freq_substring = defaultdict(int)
+        substring = s2[l : r]
+        for ch in substring:
+            freq_substring[ch] += 1
+        while r < len(s2):
+            if freq_substring == freq_s1:
+                return True
+            freq_substring[s2[l]] -= 1
+            if freq_substring[s2[l]] == 0:
+                del freq_substring[s2[l]]
+            freq_substring[s2[r]] += 1
+            l += 1
+            r += 1
+        if freq_substring == freq_s1:
+            return True
+        return False
+            
+
+        
+        
+        
+        
+"""       k = len(s1) 
+        freq_substring = defaultdict(int)
         for i in range(len(s2) - k + 1):
-            freq_substring = defaultdict(int)
             substring = s2[i : i + k]  
             for ch in substring:
                 freq_substring[ch] += 1
             if (freq_substring == freq_s1):
                 return True
         return False
-            
+"""         
