8a96114e17ef667a34020c92306f8a628dd6385d
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-04-30 13:48:12 -0700

    Time: 61 ms (84.11%) | Memory: 18.1 MB (93.18%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,34 +1,34 @@
 class Solution:
+    #USING SLIDING WINDOW TECHNIQUE + HASH-MAP 
     def minWindow(self, s: str, t: str) -> str:
-        if len(s) < len(t):
+        if len(t) > len(s):
             return ""
 
-        freq_s = [0]*128
-        freq_t = [0]*128
-
+        freq_t, window = defaultdict(int), defaultdict(int)
         for ch in t:
-            freq_t[ord(ch)] += 1
+            freq_t[ch] += 1
+        print(freq_t)
 
+        need, have = len(freq_t), 0
         l = 0
-        minLength = len(s) + 1
-        start = 0
+        minLength, minIndex = float("inf"), [-1, -1]
         for r in range(len(s)):
-            freq_s[ord(s[r])] += 1
-            while self.match_arrays(freq_s, freq_t):
-                if r - l + 1 < minLength:
-                    minLength = r - l + 1
-                    start = l
-                freq_s[ord(s[l])] -= 1
-                l += 1
-                
-        return "" if minLength > len(s) else s[start: start + minLength]
+            ch = s[r]
+            window[ch] += 1
 
-    def match_arrays(self, arr1, arr2):
-        for i in range(len(arr2)):
-            if arr2[i] == 0:
-                continue 
-            if arr1[i] >= arr2[i]:
-                continue
-            else:
-                return False 
-        return True
+            if ch in freq_t and window[ch] == freq_t[ch]:
+                have += 1
+            
+            while need == have:
+                if (r - l + 1) < minLength:
+                    minIndex = [l, r]                    
+                    minLength = r - l + 1
+                window[s[l]] -= 1 
+                if s[l] in freq_t and window[s[l]] < freq_t[s[l]]:
+                    have -= 1
+                l += 1
+        l, r = minIndex
+        return s[l : r + 1] if minLength < float("infinity") else ""
+
+#TIME-COMPLEXITY: O(N)
+#SPACE-COMPLEXITY: O(M)
