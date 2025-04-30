f66cf07c4b488331f2ac30a54025d1fd952a8df9
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-04-29 23:15:33 -0700

    Time: 968 ms (5.02%) | Memory: 18 MB (98.70%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(s) < len(t):
+            return ""
+
+        freq_s = [0]*128
+        freq_t = [0]*128
+
+        for ch in t:
+            freq_t[ord(ch)] += 1
+
+        l = 0
+        minLength = len(s) + 1
+        start = 0
+        for r in range(len(s)):
+            freq_s[ord(s[r])] += 1
+            while self.match_arrays(freq_s, freq_t):
+                if r - l + 1 < minLength:
+                    minLength = r - l + 1
+                    start = l
+                freq_s[ord(s[l])] -= 1
+                l += 1
+
+        return "" if minLength > len(s) else s[start: start + minLength]
+
+    def match_arrays(self, arr1, arr2):
+        for i in range(len(arr2)):
+            if arr2[i] > arr1[i]:
+                return False 
+        return True
