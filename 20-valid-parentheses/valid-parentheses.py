15a6909aeebe60dd5fd8c1aeddba2def8b221e19
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-05-02 19:56:11 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (96.39%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,16 +1,23 @@
 class Solution:
+    #USING STACKS 
     def isValid(self, s: str) -> bool:
         stack = []
-        mapping = {")" : "(", "}" : "{", "]" : "["}
+        mapping = {")" : "(", "]" : "[", "}" : "{"}
+        
+        if len(s) == 1:
+            return False
 
         for ch in s:
-            if ch in mapping:
-                if stack and stack[-1] == mapping[ch]:
+            if ch in mapping:   #to avoid all other invalid cases such as "){" or "}"
+                if stack and stack[-1] == mapping[ch]: #it is imp to have 2 nested loops
                     stack.pop()
                 else:
                     return False
             else:
-                stack.append(ch)
-
+                stack.append(ch) 
         return True if not stack else False
+#TIME-COMPLEXITY: O(n)
+#SPACE-COMPLEXITY: O(n)
+
+
         
