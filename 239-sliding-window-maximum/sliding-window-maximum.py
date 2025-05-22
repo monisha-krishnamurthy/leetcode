86149c7332818d6a186beba56d3a122153a5db83
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-05-22 15:13:21 -0700

    Time: 362 ms (16.61%) | Memory: 42.6 MB (6.63%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -2,31 +2,17 @@ from collections import deque
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        #USING MONOTONIC DEQUE (double sided queue)
-        result = list()
+        output = []
+        heap = [] 
+        
+        for i in range(len(nums)):
+            heapq.heappush(heap, (-nums[i], i))
+            if i >= k-1:
+                while heap[0][1] <= i-k:
+                    heapq.heappop(heap) 
+                output.append(-heap[0][0]) 
+        return output
 
-        main_q = deque() 
-        max_q = deque() 
-        for i in range(k):
-            main_q.append(nums[i])
-            while max_q and max_q[-1] < nums[i]:
-                max_q.pop()
-            max_q.append(nums[i])  
-        result.append(max_q[0])  
+#TIME-COMPLEXITY: O(n*logn) logn: push and pop operation
 
-        for i in range(1, len(nums) - k + 1): 
-            front_ele = main_q[0]
-            if front_ele == max_q[0]:
-                max_q.popleft()
-            main_q.popleft()
-            new_back_ele = nums[i+k-1] 
-            main_q.append(new_back_ele) 
-            while max_q and max_q[-1] < new_back_ele:
-                max_q.pop()
-            max_q.append(new_back_ele)
-            result.append(max_q[0])
-        return result
-#TIME-COMPLEXITY: O(n) 
-#SPACE-COMPLEXITY: O(k) 
-
-
+                
