458c3cab002419100af2ebda1997d5ffe9ae7f31
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-05-22 21:41:22 -0700

    Time: 720 ms (5.00%) | Memory: 19.5 MB (17.17%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,21 +1,24 @@
 class Solution:
+    #BRUTE-FORCE SOLUTION 
     def evalRPN(self, tokens: List[str]) -> int:
-        stack = list()
-        for i in range(len(tokens)):
-            if tokens[i] not in ('+', '-', '/', '*'): 
-                stack.append(int(tokens[i])) 
-            else:
-                operand1 = stack.pop()
-                operand2 = stack.pop()
-                result = 0
-                if tokens[i] == "+":
-                    result = operand2 + operand1
-                elif tokens[i] == "-":
-                    result = operand2 - operand1
-                elif tokens[i] == "*":
-                    result = operand2 * operand1
-                elif tokens[i] == "/":
-                    #Truncation toward zero always removes the decimal part to bring the number closer to zero, while floor division rounds the number down to the nearest integer, moving toward negative infinity.
-                    result = int(operand2 / operand1)
-                stack.append(result) 
-        return stack[-1] 
+        while len(tokens)>1:
+            for i in range(len(tokens)):
+                if tokens[i] in "+-*/":
+                    right_operand = int(tokens[i-1])
+                    left_operand = int(tokens[i-2])
+                    if tokens[i] == "+":
+                        result = left_operand + right_operand 
+                    elif tokens[i] == "-":
+                        result = left_operand - right_operand
+                    elif tokens[i] == "*":
+                        result = left_operand * right_operand
+                    elif tokens[i] == "/":
+                        result = int(left_operand / right_operand) 
+                    tokens = tokens[:i-2] + [str(result)] + tokens[i+1:] #focus
+                    break 
+        return int(tokens[0])
+
+
+#NOTE: Truncation toward zero always removes the decimal part to bring the number closer  to zero, while floor division rounds the number down to the nearest integer, moving toward negative infinity.
+#TIME-COMPLEXITY: O(n^2)
+#SPACE-COMPLEXITY: O(n^2)  
