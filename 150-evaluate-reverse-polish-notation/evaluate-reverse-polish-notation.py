c828396f4480d721550c59dffaa6a19d66fa0800
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-05-23 23:47:48 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (17.09%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,24 +1,24 @@
 class Solution:
-    #BRUTE-FORCE SOLUTION 
+    #USING STACKS
     def evalRPN(self, tokens: List[str]) -> int:
-        while len(tokens)>1:
-            for i in range(len(tokens)):
-                if tokens[i] in "+-*/":
-                    right_operand = int(tokens[i-1])
-                    left_operand = int(tokens[i-2])
-                    if tokens[i] == "+":
-                        result = left_operand + right_operand 
-                    elif tokens[i] == "-":
-                        result = left_operand - right_operand
-                    elif tokens[i] == "*":
-                        result = left_operand * right_operand
-                    elif tokens[i] == "/":
-                        result = int(left_operand / right_operand) 
-                    tokens = tokens[:i-2] + [str(result)] + tokens[i+1:] #focus
-                    break 
-        return int(tokens[0])
+        def dfs():
+            token = tokens.pop() 
+            if token not in '+-*/':
+                return int(token)
 
+            right = dfs()
+            left = dfs()
 
-#NOTE: Truncation toward zero always removes the decimal part to bring the number closer  to zero, while floor division rounds the number down to the nearest integer, moving toward negative infinity.
-#TIME-COMPLEXITY: O(n^2)
-#SPACE-COMPLEXITY: O(n^2)  
+            if token == '+':
+                return left + right
+            elif token == '-':
+                return left - right
+            elif token == '*':
+                return left * right
+            elif token == '/':
+                return int(left / right)
+        return dfs()
+
+#NOTE: Truncation toward zero always removes the decimal part to bring the number closer to zero, while floor division rounds the number down to the nearest integer, moving toward negative infinity.
+#TIME-COMPLEXITY: O(n)
+#SPACE-COMPLEXITY: O(n)
