4776f0d5d17462468a45e7968d211870f4892cb3
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-05-26 14:28:27 -0700

    Time: 43 ms (5.39%) | Memory: 18.2 MB (26.00%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,13 +1,23 @@
 class Solution:
     def generateParenthesis(self, n):
-        res = [[] for _ in range(n+1)]
-        res[0] = [""]
-        
-        for k in range(n + 1):
-            for i in range(k):
-                for left in res[i]:
-                    for right in res[k-i-1]:
-                        res[k].append("(" + left + ")" + right)
-        
-        print(res)
-        return res[-1] 
+        res = [] 
+
+        def valid(s: str):
+            open_count = 0
+            for ch in list(s):
+                open_count += 1 if ch == '(' else -1
+                if open_count < 0:
+                    return False
+            return open_count == 0 
+
+        def dfs(s: str):
+            if 2*n == len(s):
+                if valid(s):
+                    res.append(s)
+                return 
+
+            dfs(s + "(")
+            dfs(s + ")")
+
+        dfs("")
+        return res
