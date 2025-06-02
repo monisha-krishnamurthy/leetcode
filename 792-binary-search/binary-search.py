f32bf19cefb2d427fbc4cec604526b1a45051832
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-06-01 23:26:35 -0700

    Time: 0 ms (100.00%) | Memory: 18.9 MB (6.21%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,15 +1,19 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        left = 0
-        right = n-1
 
-        while left <= right:
-            mid = (left+right)//2
-            if nums[mid] > target:
-                right = mid-1
-            elif nums[mid] < target:
-                left = mid+1
-            else:
+        def recursive(left, right, nums, target):
+            if left > right:
+                return -1
+            mid = (left + right)//2
+            if nums[mid] == target:
                 return mid
-        return -1
+
+            if nums[mid] > target:
+                return recursive(left, mid-1, nums, target)
+            else:
+                return recursive(mid+1, right, nums, target)
+ 
+        return recursive(0, n-1, nums, target)
+
+
