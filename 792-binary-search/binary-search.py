2d62195e169985c61a5376f6989358fc26013d29
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-06-02 17:33:11 -0700

    Time: 0 ms (100.00%) | Memory: 19 MB (6.26%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,5 +1,5 @@
 class Solution:
-    #UPPER BOUND
+    #LOWER BOUND
 #Boundary searches refer to finding the first or last occurrence of a target value in a sorted list, while insert-point calculations refer to determining where a new value should be inserted to maintain sorted order, even if the value isn’t already present. Using binary search with "right = len(nums)" and treating the right bound as exclusive makes these operations easier because it naturally handles edge cases, like inserting beyond the last element or stopping exactly at the correct boundary, without running into off-by-one errors, since right can point just past the last index without breaking. This approach is especially useful when you need precise control over positions rather than just checking if a value exists.
     def search(self, nums: List[int], target: int) -> int:
         left  = 0
@@ -7,11 +7,11 @@ class Solution:
 
         while left < right:
             mid = left + ((right - left) // 2)
-            if nums[mid] > target:
+            if nums[mid] >= target:
                 right = mid
-            elif nums[mid] <= target:
+            elif nums[mid] < target:
                 left = mid + 1
-        return left-1 if (left and nums[left-1] == target) else -1
+        return left if (left < len(nums) and nums[left] == target) else -1
 
 #TIME-COMPLEXITY: O(logN)
 #SPACE-COMPLEXITY: O(1)
