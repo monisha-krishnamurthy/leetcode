f2c905e0d692753a84000df7c4febdf7bd3f40c1
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-06-03 23:12:24 -0700

    Time: 0 ms (100.00%) | Memory: 18.1 MB (92.88%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,7 +1,16 @@
 class Solution:
+    #STAIRCASE SEARCH
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
-                if matrix[r][c] == target:
-                    return True
+        m, n = len(matrix), len(matrix[0])
+        r, c = 0, n-1
+
+        while r < m and c >= 0:
+            if matrix[r][c] > target:
+                c -= 1
+            elif matrix[r][c] < target:
+                r += 1
+            else:
+                return True
         return False
+#TIME-COMPLEXITY: O(m+n)
+#SPACE-COMPLEXITY: O(1)
