2df6e41e4b038a371da1592bee1145ae0b952f53
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-06-06 18:12:43 -0700

    Time: 155 ms (65.99%) | Memory: 18.9 MB (75.37%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,26 +1,19 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def get_hours(piles, rate):
-            hours = 0
+        left = 1
+        right = max(piles)
+        speed = max(piles)
+
+        while left <= right:
+            mid = (left + right)//2
+            totalTime = 0
             for pile in piles:
-                hours += math.ceil(pile/rate)
-            return hours
-
-        low = 1
-        high = max(piles)
-        
-        while(low < high):
-            mid = (low+high)//2
-            hours = get_hours(piles, mid)
-
-            if hours > h:
-                low = mid + 1
-            else:
-                high = mid  
-            
-        return low
-
-
-
-
-        
+                totalTime += math.ceil(pile/mid)
+            if totalTime > h:
+                left = mid + 1
+            elif totalTime <= h:
+                speed = min(speed, mid)
+                right = mid - 1
+        return speed
+#TIME-COMPLEXITY: O(m*n), n is the length of the input array and m is the maximum number of bananas in a pile.
+#SPACE-COMPLEXITY: O(1)
