2c07fe5680fb62f8ed9306fabe4e13aaf816d578
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-06-10 14:41:08 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (83.26%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,10 +1,30 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        merged = nums1 + nums2;
-        merged.sort()
-        total_length = len(merged)
+        len1, len2 = len(nums1), len(nums2)
+        i, j = 0, 0
+        median1, median2 = 0, 0
 
-        if total_length % 2 == 0:
-            return (merged[total_length//2] + merged[(total_length//2) - 1]) / 2
+        for _ in range(((len1 + len2) // 2) + 1):
+            median2 = median1
+            if i < len1 and j < len2:
+                if nums1[i] < nums2[j]:
+                    median1 = nums1[i]
+                    i += 1
+                else:
+                    median1 = nums2[j]
+                    j += 1
+            elif i < len1:
+                median1 = nums1[i]
+                i += 1
+            elif j < len2:
+                median1 = nums2[j]
+                j += 1
+
+        if (len1 + len2) % 2 == 0:
+            return (median1 + median2) / 2.0
         else:
-            return merged[total_length // 2] 
+            return float(median1)
+
+#TIME-COMPLEXITY: O((n+m)log(n+m))
+#SPACE-COMPLEXITY: O(n+m) 
+
