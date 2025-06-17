4f50a0d188d478c60ef367fa6aa24b2997b7e720
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-06-17 19:52:50 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (14.78%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -4,24 +4,20 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        current = dummy = ListNode(0) 
-        
-        carry = 0
-        while l1 or l2 or carry:
-            value1 = l1.val if l1 else 0
-            value2 = l2.val if l2 else 0
-            
-            total = value1 + value2 + carry
-            carry = total // 10
-            digit = total % 10 
-            
-            newNode = ListNode(digit)
-            current.next = newNode
-            current = current.next
+    def add(self, l1: ListNode, l2: ListNode, carry: int):
+        if not l1 and not l2 and carry == 0:
+            return None
 
-            if l1:
-                l1 = l1.next
-            if l2:
-                l2 = l2.next
-        return dummy.next
+        v1 = l1.val if l1 else 0
+        v2 = l2.val if l2 else 0
+
+        total = v1 + v2 + carry
+        carry = total // 10
+        digit = total % 10
+
+        next_node = self.add(l1.next if l1 else None, 
+                            l2.next if l2 else None, carry) 
+        return ListNode(digit, next_node)
+
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        return self.add(l1, l2, 0)
