e324f260012424f127fe0831ccdb09713aea8f7f
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-06-19 20:48:30 -0400

    Time: 107 ms (88.34%) | Memory: 78.2 MB (90.23%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,43 +1,24 @@
-class Node:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.prev, self.next = None, None
-
 class LRUCache:
+
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}
-        self.left, self.right = Node(0,0), Node(0,0)
-        self.left.next, self.right.prev = self.right, self.left
-
-    def remove(self, node: Node):
-        previous_node, next_node = node.prev, node.next
-        previous_node.next = next_node
-        next_node.prev = previous_node
-
-    def insert(self, node: Node):
-        previous_node, next_node = self.right.prev, self.right
-        previous_node.next, next_node.prev = node, node
-        node.next, node.prev = next_node, previous_node
+        self.cache = OrderedDict()
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].value
-        return -1
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key] 
+
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.remove(self.cache[key])
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
-    
+            self.cache.move_to_end(key) 
+        self.cache[key] = value
+
         if len(self.cache) > self.capacity:
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key] 
+            self.cache.popitem(last=False)
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
