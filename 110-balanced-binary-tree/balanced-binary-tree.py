d130759b9da66aeebf4cb3f56c01368b14f401db
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-06-28 17:30:24 -0400

    Time: 0 ms (100.00%) | Memory: 18.8 MB (89.47%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -8,17 +8,16 @@ class Solution:
     def check(self, root: Optional[TreeNode]) -> bool:
         if root is None:
             return 0
-
-        checker = 1
-        lheight = self.check(root.left)
-        if lheight == -1: return -1
-        rheight = self.check(root.right)
-        if rheight == -1: return -1
         
-        if abs(lheight - rheight) > 1:
-                return -1
-        else:
-            return max(lheight,rheight) + 1
+        lh = self.check(root.left)
+        if lh == -1: return -1
+        rh = self.check(root.right)
+        if rh == -1: return -1
 
+        if abs(lh - rh) > 1:
+            return -1
+        else:
+            return max(lh, rh) + 1
+        
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         return self.check(root) != -1
