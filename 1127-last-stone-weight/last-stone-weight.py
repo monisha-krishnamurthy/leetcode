d5efa7f2a4c3e183d834ce49962586e06907242a
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-07-30 17:49:14 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (81.33%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -1,11 +1,24 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        
-        while len(stones) > 1:
-            stones.sort()
+        stones.sort()
+        n = len(stones)
+
+        while n > 1:
             curr = stones.pop() - stones.pop()
-            if curr:
-                stones.append(curr)
+            n -= 2
 
+            if curr > 0:
+                l, r = 0, n
+                while l < r:
+                    mid = (l + r) // 2
+                    if curr > stones[mid]:
+                        l = mid + 1
+                    else:
+                        r = mid
+                pos = l
+                n += 1
+                stones.append(0)
+                for i in range(n-1, pos-1, -1):
+                    stones[i] = stones[i -1]
+                stones[pos] = curr
         return stones[0] if stones else 0
-
