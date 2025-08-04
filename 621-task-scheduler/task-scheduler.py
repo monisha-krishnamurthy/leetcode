ffdfce830fab196307b733aafae6306855163f3a
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-08-03 18:59:55 -0700

    Time: 13 ms (95.14%) | Memory: 19 MB (98.22%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,18 +1,14 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        count = Counter(tasks)
-        maxHeap = [-cnt for cnt in count.values()]
-        heapq.heapify(maxHeap)
+        count = [0] * 26
+        for task in tasks:
+            count[ord(task) - ord('A')] += 1
 
-        time = 0
-        q = deque()  #[-cnt, time]
+        count.sort()
+        maxFreq = count[25]
+        idleTime = (maxFreq - 1) * n
+        for i in range(24, -1, -1):
+            idleTime -= min(maxFreq - 1, count[i]) 
+        return max(0, idleTime) +  len(tasks)
 
-        while maxHeap or q:
-            time += 1
-            if maxHeap:
-                cnt = 1 + heapq.heappop(maxHeap)
-                if cnt:
-                    q.append([cnt, time + n])
-            if q and q[0][1] == time:
-                heapq.heappush(maxHeap, q.popleft()[0])
-        return time
+
