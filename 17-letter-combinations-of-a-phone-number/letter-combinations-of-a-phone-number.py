131af6db88a736aac61dfb18e85407a265996014
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-09-22 12:44:28 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (42.25%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,17 +1,25 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        digits_map = {2:["a","b","c"], 3:["d","e","f"], 4:["g","h","i"], 5:["j","k","l"], 
-                    6:["m","n","o"], 7:["p","q","r","s"], 8:["t","u","v"], 9:["w","x","y","z"]}
-        output_list = []
-        self.recursive(list(digits), digits_map, "", output_list)
-        return output_list
+        if digits == "":
+            return []
 
-    def recursive(self, digits, digits_map, output, output_list):
-        if len(digits) == 0:
-            if output:
-                output_list.append(output) 
-            return 
+        res = []
+        digits_map = {"2":["a","b","c"], 
+                    "3":["d","e","f"], 
+                    "4":["g","h","i"], 
+                    "5":["j","k","l"], 
+                    "6":["m","n","o"], 
+                    "7":["p","q","r","s"], 
+                    "8":["t","u","v"], 
+                    "9":["w","x","y","z"]}
+        
+        def backtrack(i, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
 
-        digit = int(digits[0])
-        for alphabet in digits_map[digit]:
-            self.recursive(digits[1:], digits_map, output + alphabet, output_list)
+            for c in digits_map[digits[i]]:
+                backtrack(i+1, curStr+c)
+
+        backtrack(0,"")
+        return res
