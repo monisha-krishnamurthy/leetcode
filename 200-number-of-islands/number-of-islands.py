4efce344376442cbb911f8c811d61442e3286d8c
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-09-23 13:39:18 -0700

    Time: 303 ms (12.30%) | Memory: 20.4 MB (42.87%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,36 +1,32 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        no_of_islands = 0
-        visited = [[0]*cols for _ in range(rows)] 
-        
-        # def recursive(row, col, visted, grid):
-        #     if (row>=0 and row<len(grid) and col>=0 and col<len(grid[0]) and grid[row][col] == '1' and visited[row][col] == 0):
-        #         visited[row][col] = 1
-        #         recursive(row+1, col, visited, grid)
-        #         recursive(row, col+1, visited, grid)
-        #         recursive(row-1, col, visited, grid)
-        #         recursive(row, col-1, visited, grid)
-        #     return
+        rows, cols = len(grid), len(grid[0])
+        noOfIslands = 0
+        visited = [[False] * cols for _ in range(rows)]
 
-        def bfs(row, col):
+        def bfs(r,c):
             queue = collections.deque()
-            queue.append((row,col))
-            visited[row][col] = 1
-            while queue:
-                r, c = queue.popleft()
-                directions = [[1,0], [-1,0], [0,1], [0,-1]]
-                for dr, dc in directions:
-                    row = r + dr
-                    col = c + dc
-                    if (row in range(rows) and col in range(cols) and grid[row][col]=='1' and visited[row][col]==0):
-                        queue.append((row,col))
-                        visited[row][col] = 1
+            queue.append((r,c))
+            visited[r][c] = True
 
-        for i in range(rows):
-            for j in range(cols):
-                if (grid[i][j] == '1' and visited[i][j] == 0):
-                    bfs(i, j)
-                    no_of_islands += 1
-        return no_of_islands
+            while queue:
+                row,col = queue.popleft()
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                for dr, dc in directions:
+                    new_row, new_col = row + dr, col + dc
+                    if (new_row in range(rows) and new_col in range(cols)
+                        and not visited[new_row][new_col] and grid[new_row][new_col] == '1'):
+                        queue.append((new_row,new_col))
+                        visited[new_row][new_col] = True
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1' and not visited[r][c]:
+                    bfs(r,c)
+                    noOfIslands += 1
+        return noOfIslands
+
+
+
+    
+
