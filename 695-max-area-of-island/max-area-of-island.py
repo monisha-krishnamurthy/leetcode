93c397b01cac6dbe9891e733536f358f3505cc99
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2025-11-12 13:32:29 -0700

    Time: 35 ms (22.28%) | Memory: 18.6 MB (34.45%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,30 +1,18 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        visited = [[False] * cols for _ in range(rows)]
-        maxArea = 0
+        visited= [[False] * cols for _ in range(rows)]
+        area = 0
 
-        def bfs(r, c):
-            queue = collections.deque()
-            queue.append((r,c))
+        def dfs(r,c):
+            if r not in range(rows) or c not in range(cols) or grid[r][c] == 0 or visited[r][c] == True:
+                return 0
+
             visited[r][c] = True
-            area = grid[r][c]
-
-            while queue:
-                (row, col) = queue.popleft()
-                directions = [[0,1],[0,-1],[1,0],[-1,0]]
-                for dr,dc in directions:
-                    new_row, new_col = row + dr, col + dc
-                    if (new_row in range(rows) and new_col in range(cols) and grid[new_row][new_col] == 1 and visited[new_row][new_col] == False):
-                            area += grid[new_row][new_col]
-                            visited[new_row][new_col] = True
-                            queue.append((new_row,new_col))
-            return area
-
+            return (1 + dfs(r+1, c) + dfs(r-1, c) + dfs(r, c+1) + dfs(r, c-1))
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 1 and visited[r][c] == False:
-                    area = bfs(r,c)
-                    maxArea = max(maxArea, area)
-        return maxArea
+                if grid[r][c] == 1 and visited[r][c] != True:
+                    area = max(area, dfs(r,c))
+        return area  
