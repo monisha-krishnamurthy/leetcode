4715cdbf71252bee3308c0794ccaf6e0a263192c
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2026-01-25 10:18:01 -0700

    Time: 9 ms (30.27%) | Memory: 22.3 MB (37.51%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -4,28 +4,30 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         ROWS, COLS = len(board), len(board[0])
+        directions = [[1,0], [-1,0], [0,1], [0,-1]]
 
-        def marker(r,c):
-            if (c<0 or r<0 or c==COLS or r==ROWS or board[r][c] != 'O'):
-                return
-            board[r][c] = 'T'
-            marker(r+1, c)
-            marker(r-1, c)
-            marker(r, c+1)
-            marker(r, c-1)
-
-        for r in range(ROWS):
-            marker(r, 0)
-            marker(r, COLS-1)
-
-        for c in range(COLS):
-            marker(0, c)
-            marker(ROWS-1, c)
+        q = deque()
 
         for r in range(ROWS):
             for c in range(COLS):
-                if board[r][c] == 'O':
-                    board[r][c] = 'X'
+                if (r==0 or r==ROWS-1 or c==0 or c==COLS-1) and board[r][c] == 'O':
+                    q.append((r,c))
+                    
+
+        while q:
+            r,c = q.popleft()  
+            board[r][c] = 'T'
+            for dr, dc in directions:
+                nr, nc = dr + r, dc + c
+                if (0 <= nr < ROWS and 0 <= nc < COLS) and board[nr][nc] == 'O':
+                    q.append((nr,nc))
+
+        for r in range(ROWS):
+            for c in range(COLS):
                 if board[r][c] == 'T':
                     board[r][c] = 'O'
-                
+                elif board[r][c] == 'O':
+                    board[r][c] = 'X'        
+
+
+  
