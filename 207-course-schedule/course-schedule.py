87b2bab49f133c67d57513d4f47c95098d291e4c
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2026-01-27 07:58:04 -0700

    Time: 7 ms (45.21%) | Memory: 21.4 MB (11.05%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,56 +1,27 @@
-# class Solution:
-#     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-#         adj_list = { i:[] for i in range(numCourses) }
-#         for course, pre in prerequisites:
-#             adj_list[course].append(pre)
-        
-#         visitSet = set()
-
-#         def dfs(course):
-#             if course in visitSet:
-#                 return False
-#             if adj_list[course] == []:
-#                 return True
-            
-#             visitSet.add(course)
-#             for pre in adj_list[course]:
-#                 if not dfs(pre):
-#                     return False
-#             visitSet.remove(course)
-#             adj_list[course] == []
-#             return True
-
-#         for course in range(numCourses):
-#             if not dfs(course):
-#                 return False
-#         return True 
-
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # Map each course to its prerequisites
-        preMap = {i: [] for i in range(numCourses)}
-        for crs, pre in prerequisites:
-            preMap[crs].append(pre)
+        adj_list = { i:[] for i in range(numCourses) }
+        for course, pre in prerequisites:
+            adj_list[course].append(pre)
+        
+        visitSet = set()
 
-        # Store all courses along the current DFS path
-        visiting = set()
-
-        def dfs(crs):
-            if crs in visiting:
-                # Cycle detected
+        def dfs(course):
+            if course in visitSet:
                 return False
-            if preMap[crs] == []:
+            if adj_list[course] == []:
                 return True
-
-            visiting.add(crs)
-            for pre in preMap[crs]:
+            
+            visitSet.add(course)
+            for pre in adj_list[course]:
                 if not dfs(pre):
                     return False
-            visiting.remove(crs)
-            preMap[crs] = []
+            visitSet.remove(course)
+            adj_list[course] = []
             return True
 
-        for c in range(numCourses):
-            if not dfs(c):
+        for course in range(numCourses):
+            if not dfs(course):
                 return False
-        return True
+        return True 
+
