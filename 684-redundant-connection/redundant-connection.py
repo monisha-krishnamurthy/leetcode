039937bdbd042a16c0e61a7c00dc69e36d1f4064
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2026-02-07 14:01:40 -0700

    Time: 3 ms (54.29%) | Memory: 19.6 MB (44.66%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,26 +1,28 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         n = len(edges)
-        adj_list = [[] for _ in range(n+1)]
-
-        def dfs(node, parent):
-            if visited[node]:
-                return True
-
-            visited[node] = True
-            for neigh in adj_list[node]:
-                if neigh == parent:
-                    continue
-                if dfs(neigh, node):
-                    return True
-            return False 
-
+        adj = [[] for _ in range(n+1)]
+        indegree = [0] * (n+1)
         for u,v in edges:
-            adj_list[u].append(v)
-            adj_list[v].append(u)
-            visited = [False] * (n+1)
+            adj[u].append(v)
+            adj[v].append(u)
+            indegree[u] += 1
+            indegree[v] += 1
 
-            if dfs(u, -1):
+        q = deque()
+        for i in range(1, n+1):
+            if indegree[i] == 1:
+                q.append(i)
+
+        while q:
+            node = q.popleft()
+            indegree[node] -= 1
+            for neigh in adj[node]:
+                indegree[neigh] -= 1
+                if indegree[neigh] == 1:
+                    q.append(neigh)
+
+        for u,v in reversed(edges):
+            if indegree[u] == 2 and indegree[v]:
                 return [u,v]
         return []
-
