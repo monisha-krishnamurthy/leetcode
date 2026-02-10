cd3f1eadc69befa8419df824f7c68057c778e1ed
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2026-02-10 12:44:19 -0700

    Time: 0 ms (100.00%) | Memory: 19.5 MB (11.59%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,31 +1,12 @@
 class Solution:
-    #TOP DOWN APPROACH i.e., MEMOIZATION
+    #BOTTOM-UP APPROACH i.e., TABULATION
     def climbStairs(self, n: int) -> int:
-        cache = [-1]*n
-        def dfs(i):
-            if i >= n:
-                return i == n
-            if cache[i] != -1:
-                return cache[i]
-            cache[i] = dfs(i+1) + dfs(i+2)
-            return cache[i]
-
-        return dfs(0)   #how does it internally compute everything to finally give the
-                        #output at dfs(0)
-
-"""
-dfs(0)
-├── dfs(1)
-│   ├── dfs(2)
-│   │   ├── dfs(3) → 1
-│   │   └── dfs(4) → 0
-│   │   → cache[2] = 1
-│   └── dfs(3) → 1
-│   → cache[1] = 2
-└── dfs(2) → cache hit → 1
-
-→ cache[0] = 3
-"""
-
+        if n<=2:
+            return n
+        dp = [0] * (n+1)
+        dp[1], dp[2] = 1, 2
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
 
         
