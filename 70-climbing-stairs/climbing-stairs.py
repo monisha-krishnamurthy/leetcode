97dac3a5b618393eef000a410adf7c497346c595
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2026-02-10 12:31:08 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (53.37%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -10,6 +10,21 @@ class Solution:
             return cache[i]
 
         return dfs(0)   #how does it internally compute everything to finally give the
+                        #output at dfs(0)
+
+"""
+dfs(0)
+├── dfs(1)
+│   ├── dfs(2)
+│   │   ├── dfs(3) → 1
+│   │   └── dfs(4) → 0
+│   │   → cache[2] = 1
+│   └── dfs(3) → 1
+│   → cache[1] = 2
+└── dfs(2) → cache hit → 1
+
+→ cache[0] = 3
+"""
 
 
         
