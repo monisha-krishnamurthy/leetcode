a0d5925bbe8346738c13d7cf1a00fe48e80cd1b9
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2026-02-11 12:37:07 -0700

    Time: 3 ms (67.72%) | Memory: 19.3 MB (55.73%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,9 +1,7 @@
 class Solution:
-    #BOTTOM-UP APPROACH / TABULATION
+    #BOTTOM-UP APPROACH / TABULATION.  [SPACE-OPTIMISED]
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        dp = [0]*(n+1)
-        for i in range(2, n+1):
-            dp[i] = min(dp[i-1] + cost[i-1], 
-                        dp[i-2] + cost[i-2])
-        return dp[n] 
+        for i in range(len(cost)-3, -1, -1):
+            cost[i] += min(cost[i+1], cost[i+2]) 
+
+        return min(cost[0], cost[1]) 
