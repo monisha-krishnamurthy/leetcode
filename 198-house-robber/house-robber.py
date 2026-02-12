8e7c96b70ba7e7b5cfe771f66f4475c0f02ec027
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2026-02-12 13:55:11 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (42.19%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,15 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+
         n = len(nums)
-        dp = [0] * (n+1)
+        dp = [0]*n
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        return dp[-1]
 
-        dp[0] = 0
-        dp[1] = nums[0]
 
-        #dp at every index will store an intermediary value upto that index, 
-        #may or maynot be inclusive 
-        #[1 100 20] dp = [1 100 100]
-
-        for i in range(2, n+1):
-            dp[i] = max(dp[i-1], dp[i-2]+nums[i-1]) 
-        return dp[-1] 
