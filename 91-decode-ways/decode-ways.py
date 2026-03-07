1b0ae8c05c56a88d8144a281c8c7fef3447adeb3
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2026-03-07 10:20:02 -0700

    Time: 0 ms (100.00%) | Memory: 19.5 MB (22.56%) - LeetSync

---
+++ 91-decode-ways/decode-ways.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = {len(s) : 1}
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+            if s[i] == '0':
+                return 0
+
+            result = dfs(i+1)
+            if i < len(s) - 1:
+                if (s[i] == '1' or s[i] == '2' and s[i+1] in '0123456'):
+                    result += dfs(i+2)
+            dp[i] = result
+            return result
+        
+        return dfs(0)
+
