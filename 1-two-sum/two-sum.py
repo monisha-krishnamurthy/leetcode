0c7d95726e9c5bafae3e6b0ab23b88e10c9a84de
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2026-04-14 11:53:44 -0700

    Time: 0 ms (100.00%) | Memory: 20.5 MB (25.90%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,19 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        A = []
+        mapping = dict()
         for i, num in enumerate(nums):
-            A.append([num, i])
-
-        A.sort()
-        i = 0
-        j = len(nums) - 1
-        while i < j:
-            summ = A[i][0] + A[j][0]
-            if summ == target:
-                return (min(A[i][1], A[j][1]), max(A[i][1], A[j][1]))
-            elif summ > target :
-                j -= 1
-            else:
-                i += 1
-        return []
-
+            diff = target - num
+            if diff in mapping:
+                return [i, mapping[diff]]
+            mapping[num] = i
