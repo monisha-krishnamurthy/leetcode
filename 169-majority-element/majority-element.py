028bac8979e8385e30e7f0c28f00616df8840d31
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2026-04-14 15:30:18 -0700

    Time: 11 ms (32.82%) | Memory: 21.1 MB (91.00%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,5 +1,7 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        mid = int(len(nums)/2)
-        return nums[mid]
+        mapping = dict()
+        for num in nums:
+            mapping[num] = mapping.get(num,0) + 1
+
+        return max(mapping, key=mapping.get)
