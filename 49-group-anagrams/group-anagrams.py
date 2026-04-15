63428937a6ce3c56ade3b18cfda300a1a399b540
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2026-04-15 13:10:33 -0700

    Time: 11 ms (84.54%) | Memory: 22.1 MB (59.43%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,28 +1,8 @@
 class Solution:
-    #given a string, find a freq array of its characters
-    def freqArray(self, s1: str):
-        #initialise char array to an empty array of size 26
-        chArray = [0]*26
-        #iterate through the string
-        for ch in s1:
-            #convert the char to an index and increment the corresponding value at the index  
-            chArray[ord(ch) - ord('a')] += 1
-        return chArray
-
-    #method to group strings that are anagrams of each other
-    def groupAnagrams(self, arr: List[str]) -> List[List[str]]:
-        #initialise a hashmap to store the char freq as key and the string as value
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         mapping = defaultdict(list)
-        #iterate over the strings in the input array
-        for str in arr:
-            #convert array to its freq list
-            charMap = self.freqArray(str)
-            #appending to the hashmap
-            mapping[tuple(charMap)].append(str)
-
-        output = list()
-    #iterate over the values of hashmap to return the result
-        for value in mapping.values():
-            output.append(tuple(value))
-        return output	
+        for string in strs:
+            sortedS = ''.join(sorted(string))
+            mapping[sortedS].append(string)
+        return list(mapping.values())
 
