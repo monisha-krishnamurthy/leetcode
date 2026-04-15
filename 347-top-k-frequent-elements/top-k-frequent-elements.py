dab92f745461bdfdaee3f1cbe9108c25228f1f60
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2026-04-15 13:56:42 -0700

    Time: 4 ms (67.09%) | Memory: 22.9 MB (66.58%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,18 +1,7 @@
 class Solution:
-    # USING FREQ-MAP AND BUBBLE SORT (FREQ ARRAY)
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq_map = defaultdict(int)
-        count_arr = [[] for i in range(len(nums) + 1)]
-        
+        mapping = defaultdict(int)
         for num in nums:
-            freq_map[num] += 1 
-
-        for key,val in freq_map.items():
-            count_arr[val].append(key)
-
-        output = list()
-        for i in range(len(count_arr)-1, 0, -1):
-            for ele in count_arr[i]:
-                output.append(ele)
-                if len(output) == k:
-                    return output
+            mapping[num] += 1
+        sorted_dict = sorted(mapping.items(), key=lambda item: item[1], reverse = True)
+        return [key for key, val in sorted_dict[:k]]
