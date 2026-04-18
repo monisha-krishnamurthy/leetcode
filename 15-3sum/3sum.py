d0c23fa8825c1c93691bff14a42664d0496bbca8
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2026-04-17 17:35:31 -0700

    Time: 954 ms (17.76%) | Memory: 22.1 MB (83.84%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,44 +1,19 @@
 class Solution:
-    def threeSum(self, array: List[int]) -> List[List[int]]:
-        #sort the given array
-        array.sort()
-        print(array)
-        #build a mapping of values to count
-        mapping = defaultdict(int)
-        for val in array:
-            mapping[val] += 1
-        #initialise the result array
-        result = []
-
-        #scan through array for the first number
-        for i in range(len(array) - 2):
-            #make sure the number is not the same as the previous
-            if i > 0 and array[i] == array[i-1]:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        output = list()
+        for i in range(len(nums)):
+            if i>0 and nums[i]== nums[i-1]:
                 continue
-            #decrement count of array[i] in mapping
-            mapping[array[i]] -= 1
-
-            #looking for the second number
-            for j in range(i+1, len(array) -1):
-                #make sure the number is not the same as the previous
-                if j > i+1 and array[j] == array[j-1]:
-                    continue
-                #decrement count of array[j] in mapping
-                mapping[array[j]] -= 1
-                #find the complement in set 
-                complement = 0 - array[i] - array[j]
-                #complement should not be less than array[i] and array[j]
-                if complement < array[i] or complement < array[j]:
-                    mapping[array[j]] += 1
-                    continue
-                if mapping[complement] > 0:
-                    #found 1 answer set
-                    #add to the list
-                    result.append([array[i], array[j], complement])
-                mapping[array[j]] += 1 
-                #return result after finding all combinations
-        return result
-
-
-
-
+            j, k = i+1, len(nums)-1
+            while j<k:
+                if nums[i] + nums[j] + nums[k] == 0:
+                    output.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    while nums[j] == nums[j-1] and j<k:
+                        j +=1
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
+                elif nums[i] + nums[j] + nums[k] < 0:
+                    j += 1
+        return output    
