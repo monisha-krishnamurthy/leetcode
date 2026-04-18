58538a72ba18308e719ef4405b289c6f79c3db76
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2026-04-18 16:44:08 -0700

    Time: 55 ms (43.68%) | Memory: 28.7 MB (48.12%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,10 @@
 class Solution:
-    #USING DYNAMIC PROGRAMMING
     def maxProfit(self, prices: List[int]) -> int:
-        max_profit = 0
-        minBuy = prices[0]
-
-        #same logic as the sliding window solution
-        for i in range(len(prices)):
-            max_profit = max(max_profit, prices[i]-minBuy)
-            minBuy = min(minBuy, prices[i])        
-        return max_profit
-#TIME-COMPLEXITY: O(n)
-#SPACE-COMPLEXITY: O(1)
-
+        buy = prices[0]
+        profit = 0
+        for i in range(1, len(prices)):
+            if buy > prices[i]:
+                buy = prices[i]
+            else:
+                profit = max(profit, prices[i]-buy)
+        return profit
