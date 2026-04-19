d24a2d23c2f66ebc68489bd6a9d4e8a427f6fa50
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2026-04-18 18:01:59 -0700

    Time: 111 ms (39.08%) | Memory: 19.5 MB (88.43%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,23 +1,14 @@
 class Solution:
-    #USING SLIDING WINDOW TECHNIQUE (TWO-POINTER APPROACH)
     def characterReplacement(self, s: str, k: int) -> int:
-        maxLength = 0
-        freq_dict = defaultdict(int)
-        l = 0
-
-        for r in range(len(s)):
-            freq_dict[s[r]] += 1
-
-            if (r - l + 1) - max(freq_dict.values()) > k:
-                freq_dict[s[l]] -= 1
-                l += 1
-
-            maxLength = max(maxLength, r - l + 1)
-        return maxLength
-#TIME-COMPLEXITY: O(n)
-#SPACE-COMPLEXITY: O(m)
-
-
-
-
- 
+        mapping = defaultdict(int)
+        maxlength = 0
+        i, j = 0, 0
+        for j in range(len(s)):
+            mapping[s[j]] += 1
+            length = j-i+1
+            maxFreq = max(mapping.values())
+            if length > maxFreq+k:
+                mapping[s[i]] -= 1
+                i += 1
+            maxlength = max(j-i+1, maxlength)
+        return maxlength
