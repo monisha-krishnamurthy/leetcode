b83aa2bb103769f8ac82aa6492ed2e5a2ae5fc84
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2026-06-11 14:00:45 -0700

    Time: 11 ms (76.29%) | Memory: 19.4 MB (44.37%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,15 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # dict() vs defaultdict(): explicit control over missing keys Vs cleaner code w/o manual checks
-        mapping = defaultdict(int) 
-
-        for char in s:
-            mapping[ord(char) - ord('a')] += 1
-        
-        for char in t:
-            mapping[ord(char) - ord('a')] -= 1
-
-        for values in mapping.values():
-            if values != 0:
+        mapping_S = defaultdict(int)
+        for ch in s:
+            mapping_S[ch] += 1
+        for ch in t:
+            mapping_S[ch] -= 1
+        print(mapping_S)
+        for i in mapping_S.values():
+            if i != 0:
                 return False
         return True
