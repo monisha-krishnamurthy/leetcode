bded2142060ba5da45b96a6f2fb7a8c2dc018b66
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2026-06-11 14:02:41 -0700

    Time: 15 ms (43.06%) | Memory: 19.3 MB (75.31%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,9 +2,9 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         mapping_S = defaultdict(int)
         for ch in s:
-            mapping_S[ch] += 1
+            mapping_S[ord(ch) - ord('a')] += 1
         for ch in t:
-            mapping_S[ch] -= 1
+            mapping_S[ord(ch) - ord('a')] -= 1
         print(mapping_S)
         for i in mapping_S.values():
             if i != 0:
