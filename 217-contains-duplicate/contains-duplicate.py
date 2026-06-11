9134032492f9640a7213a89a0b2fc78a59a4b7df
MONISHA KRISHNAMURTHY <106053533+monisha-krishnamurthy@users.noreply.github.com>
2026-06-11 13:53:40 -0700

    Time: 31 ms (13.39%) | Memory: 36.4 MB (6.55%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -2,8 +2,7 @@ class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         mapping = defaultdict(int)
         for num in nums:
-            mapping[num] += 1
-        for i in mapping.values(): 
-            if i > 1:
+            mapping[num] += 1 
+            if mapping[num] > 1:
                 return True
         return False
